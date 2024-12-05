422a6299ccdde73ab55cae144de8a19260a0f8fd
saikurelli <22625395+saikurelli@users.noreply.github.com>
2024-12-05 10:14:13 -0600

    Adding word mode preview for guests

--- hideshare/src/web/extension.ts
+++ hideshare/src/web/extension.ts
@@ -37,7 +37,7 @@ export async function activate(context: vscode.ExtensionContext) {
   // }
   // TODO: Get the reveal method from the host - change with notification service
   // assume line level first
-  let extensionMode = 'line';
+  let extensionMode = await vscode.window.showInformationMessage('Select a HideShare mode', 'real-time', 'line', 'word');
   // context.subscriptions.push(
   //   vscode.commands.registerCommand('hideshare.requestBlock', () => {
   //     sendNotificationToHost(JSON.stringify('Please switch to block mode'));
@@ -68,35 +68,78 @@ export async function activate(context: vscode.ExtensionContext) {
   // Keep track of indentation levels
   const tabs: number[] = [];
 
-  // Trigger the decoration update based on text changes or cursor position
-  const onDidChangeTextDocument = vscode.workspace.onDidChangeTextDocument(async (event) => {
-    // java ls
-    for (const editor of vscode.window.visibleTextEditors) {
-      let activeLine = editor.selection.active.line;
-      event.contentChanges.forEach(change => {
-        let line = change.range.start.line;
-        // TODO: update to just edit line
-        editor.setDecorations(slightlyTransparentDecorationType, [new vscode.Range(line, 0, line, editor.document.lineAt(line).range.end.character)]);
-      });
+  const changeModeLine = vscode.commands.registerCommand('hideshare.enableLineMode', () => {
+    vscode.window.showInformationMessage('Changed to Line Mode!');
+    extensionMode = 'line';
+  });
+  context.subscriptions.push(changeModeLine);
 
-      // Remove decoration from the previous line when moving to a new line
-      if (lastActiveLine !== -1 && lastActiveLine !== activeLine) {
-        removeLineDecoration(editor, slightlyTransparentDecorationType);
-      }
-
-      // Update last active line to the current line
-      lastActiveLine = activeLine;
-    }
-    context.subscriptions.push(onDidChangeTextDocument);
+  const changeModeBlock = vscode.commands.registerCommand('hideshare.enableWordMode', () => {
+    vscode.window.showInformationMessage('Changed to Word Mode!');
+    extensionMode = 'word';
   });
 
-  // Remove the decoration from the file (used when moving to next line)
-  function removeLineDecoration(
-    editor: vscode.TextEditor,
-    hiddenDecorationType: vscode.TextEditorDecorationType,
-  ) {
-    editor.setDecorations(hiddenDecorationType, []); // Clear hidden text decoration (" If rangesOrOptions is empty, the existing decorations with the given decoration type will be removed.")
+  // Trigger the decoration update based on text changes or cursor position
+  const onDidChangeTextDocument = vscode.workspace.onDidChangeTextDocument(async (event) => {
+
+    if (extensionMode === 'line') {
+      for (const editor of vscode.window.visibleTextEditors) {
+        let activeLine = editor.selection.active.line;
+        event.contentChanges.forEach(change => {
+          let line = change.range.start.line;
+          editor.setDecorations(slightlyTransparentDecorationType, [new vscode.Range(line, 0, line, editor.document.lineAt(line).range.end.character)]);
+        });
+
+        // Remove decoration from the previous line when moving to a new line
+        if (lastActiveLine !== -1 && lastActiveLine !== activeLine) {
+          removeLineDecoration(editor, slightlyTransparentDecorationType);
+        }
+
+        // Update last active line to the current line
+        lastActiveLine = activeLine;
+      }
+    }
+    else if (extensionMode === 'word') {
+      for (const editor of vscode.window.visibleTextEditors) {
+        let activeLine = editor.selection.active.line;
+        event.contentChanges.forEach(change => {
+          let line = change.range.start.line;
+          hideLine(line, 'word', editor, slightlyTransparentDecorationType);
+        }
+        );
+      }
+    }
+  });
+  context.subscriptions.push(onDidChangeTextDocument);
+}
+
+
+// Remove the decoration from the file (used when moving to next line)
+function removeLineDecoration(
+  editor: vscode.TextEditor,
+  hiddenDecorationType: vscode.TextEditorDecorationType,
+) {
+  editor.setDecorations(hiddenDecorationType, []); // Clear hidden text decoration (" If rangesOrOptions is empty, the existing decorations with the given decoration type will be removed.")
+}
+
+
+// Apply hide line text to the current line
+function hideLine(
+  lineNumber: number,
+  mode: string,
+  editor: vscode.TextEditor,
+  hiddenDecorationType: vscode.TextEditorDecorationType,
+) {
+  let lineRange;
+  if (mode === 'line') {
+    lineRange = editor.document.lineAt(lineNumber).range;
+  } else {
+    let startIndex = new vscode.Position(lineNumber, editor.document.lineAt(lineNumber).text.lastIndexOf(' '));
+    lineRange = new vscode.Range(startIndex, editor.document.lineAt(lineNumber).range.end);
   }
+
+  // Apply the hidden text decoration (make text transparent) to the line
+  editor.setDecorations(hiddenDecorationType, [lineRange]);
 }
 
 export function deactivate() { }
